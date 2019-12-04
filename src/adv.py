8716bda03d3f9fafd2ee172540034ab9631a06a8
Armando Roman <armandoroman1016@gmail.com>
2019-12-04 15:06:55 -0800

    day 1 mvp complete

--- src/adv.py
+++ src/adv.py
@@ -37,15 +37,53 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 
+from player import Player
+import textwrap 
+
 # Make a new player object that is currently in the 'outside' room.
+print("-----------------------------------------------------------------------------------------------------")
+playerName = input('Welcome to Dark Island, to get started enter your name => ')
+player = Player(playerName, room['outside'])
+
+# Player instructions
+print("-----------------------------------------------------------------------------------------------------")
+print("To navigate around the island enter 'n', 's', 'e', 'w', to look for the treasure or enter 'q' to quit.")
+print("-----------------------------------------------------------------------------------------------------")
+
+
+wrapper = textwrap.TextWrapper(width=100)
+
+allowed_moves = ['n', 's', 'e', 'w', 'q']
+
+last_room = None
 
 # Write a loop that:
-#
+while True:
 # * Prints the current room name
+    print(player.current_room.name)
 # * Prints the current description (the textwrap module might be useful here).
+    print(wrapper.fill(player.current_room.description))
 # * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+    direction = input('Where shall you proceed next?')
+    # Checking to see if user entered an allowed move.
+    if direction.lower() in allowed_moves:
+        # If the user enters "q", quit the game.
+        if direction.lower() == 'q':
+            print(f'Goodbye {player.name}')
+            break
+        else:
+        # If the user enters a cardinal direction, attempt to move to the room there.
+            selected_room = player.current_room.__getattribute__(f'{direction.lower()}_to')
+            #If the direction is valid update the players current room
+            if selected_room != None:
+                # updating room
+                player.current_room = selected_room
+                print("-----------------------------------------------------------------------------------------------------")
+            # Print an error message if there isn't a location in the direction specied
+            elif selected_room == None:
+                print(f"There isn't a thing in that direction, {player.name}")
+                print("-----------------------------------------------------------------------------------------------------")
+    # Print an error message if the movement isn't allowed.
+    else:
+        print("-----------------------------------------------------------------------------------------------------")
+        print("Enter a valid direction or 'q' to quit")
